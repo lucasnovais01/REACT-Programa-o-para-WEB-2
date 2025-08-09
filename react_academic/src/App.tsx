2b8fbbb2938ce019b848a723b788acc7dabecd26
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-08-08 22:16:44 -0300

    AULA_DA_SEXTA

--- react_academic/src/App.tsx
+++ react_academic/src/App.tsx
@@ -1,29 +1,61 @@
 //import { Fragment } from "react/jsx-runtime" // está errado, o correto é o que está abaixo:
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState, type FormEvent } from "react";
 
 function App() {
 
   const [nome, setNome] = useState<string>("");
   const [cidade, setCidade] = useState<string>("");
+
+  const[email, setEmail] = useState<string>("");
 //
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+//
+
+  useEffect(()=>{
+    inputRef.current?.focus();
+  },[])
+
+  useEffect(()=>{
+    console.log("O campo email foi alterado");
+  }, [email])
 
 
-  useEffect(() => {
-    console.log("A cidade foi atualizada");
-  }, [cidade]);
+  const handleSubmit = (e:FormEvent) => {
 
-  console.log(nome);
+    
+    e.preventDefault();
+
+    console.log("Nome" +nome);
+    console.log("Cidade "+cidade);
+    console.log("E-mail "+email);
+    console.log("idade "+inputRef.current?.value);
+
+    console.log("Registro gravado com sucesso");
+  }
+
 
   return (
-    <>
-      <label>Nome: </label>
-      <input type="text" value={nome} onChange={(e) => setNome(e.target.value) }/>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Nome: </label>
+        <input type="text" value={nome} onChange={(e) => setNome(e.target.value) }/>
 
-      <label> Cidade: </label>
-      <input type="text" value={cidade} onChange={(e) => setCidade(e.target.value) }/>
-    </>
+        <label> Cidade: </label>
+        <input type="text" value={cidade} onChange={(e) => setCidade(e.target.value) }/>
+
+        <label> Idade: </label>
+        <input type="text" ref={inputRef}/>
+
+
+        <label> Email: </label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value) }/>
+
+        <label> Enviar </label>
+        <input type="button"></input>
+
+      </form>
+    </div>
   )
 }
 
