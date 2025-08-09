f88e5eefe3158663cdcaba706dfa5231396a7d9c
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-08-08 22:14:55 -0300

    enviado

--- react_academic/src/App.tsx
+++ react_academic/src/App.tsx
@@ -1,10 +1,28 @@
+//import { Fragment } from "react/jsx-runtime" // está errado, o correto é o que está abaixo:
 
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [nome, setNome] = useState<string>("");
+  const [cidade, setCidade] = useState<string>("");
+//
+  const inputRef = useRef(null);
+
+
+  useEffect(() => {
+    console.log("A cidade foi atualizada");
+  }, [cidade]);
+
+  console.log(nome);
+
   return (
     <>
-      <div>Lucas</div>
+      <label>Nome: </label>
+      <input type="text" value={nome} onChange={(e) => setNome(e.target.value) }/>
+
+      <label> Cidade: </label>
+      <input type="text" value={cidade} onChange={(e) => setCidade(e.target.value) }/>
     </>
   )
 }
